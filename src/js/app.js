510d6d815af4ad626c0afd2d1290dfabbbc4f799
Mateusz <michalikkontakt@gmail.com>
2022-04-02 17:54:55 +0200

    changed all styles and finished basic version of project

--- src/js/app.js
+++ src/js/app.js
@@ -14,14 +14,13 @@ const result = document.querySelector('.result')
 
 const historyArea = document.querySelector('.history-container__notes-area')
 const emptyError = document.querySelector('.empty-error')
+const clearHistoryBtn = document.querySelector('.history-container__clear-all-btn')
 let cardID = 0
 let cardAmount = 0
 let currency1
 let currency2
 let date
 let hour
-
-// -----------------------------------------
 
 const main = () => {
     events()
@@ -33,9 +32,16 @@ const events = () => {
     dateCalculating()
     saveBtn.addEventListener('click', createElement)
     inputOne.addEventListener('input', calculate)
-    currencyChoiceOne.addEventListener('change', calculate)
     swapBtn.addEventListener('click', swap)
-    currencyChoiceTwo.addEventListener('change', calculate)
+    currencyChoiceOne.addEventListener('change', () => {
+        calculate()
+        checkIfTheSame()
+    })
+    currencyChoiceTwo.addEventListener('change', () => {
+        calculate()
+        checkIfTheSame()
+    })
+
     themeBtn.addEventListener('click', () => {
         if (!themeCircle.classList.contains('dark-mode-transform')) {
             themeCircle.classList.add('dark-mode-transform')
@@ -49,8 +55,39 @@ const events = () => {
             document.body.setAttribute('data-theme', 'dark')
         }
     })
+    clearHistoryBtn.addEventListener('click', () => {
+        historyArea.textContent = ''
+        cardID = 0
+        cardAmount = 0
+        emptyChecker()
+        showClearBtn()
+    })
 }
 
+const checkIfTheSame = () => {
+    if (currencyChoiceOne.value === currencyChoiceTwo.value) {
+        saveBtn.style.pointerEvents = 'none'
+        saveBtn.style.opacity = '0.5'
+        swapBtn.style.pointerEvents = 'none'
+        swapBtn.style.opacity = '0.5'
+    } else {
+        saveBtn.style.pointerEvents = 'all'
+        saveBtn.style.opacity = '1'
+        swapBtn.style.pointerEvents = 'all'
+        swapBtn.style.opacity = '1'
+    }
+}
+
+const showClearBtn = () => {
+    if (cardAmount !== 0) {
+        clearHistoryBtn.style.visibility = 'visible'
+        clearHistoryBtn.style.opacity = '1'
+    } else {
+        clearHistoryBtn.style.visibility = 'hidden'
+        clearHistoryBtn.style.opacity = '0'
+    }
+
+}
 
 const createElement = () => {
     const historyNote = document.createElement('div')
@@ -72,6 +109,7 @@ const createElement = () => {
     cardID++
     cardAmount++
     emptyChecker()
+    showClearBtn()
 }
 
 const deleteElement = (id) => {
@@ -80,8 +118,10 @@ const deleteElement = (id) => {
 
     if (cardAmount !== 0) {
         cardAmount--
+        cardID--
     }
     emptyChecker()
+    showClearBtn()
 }
 
 const emptyChecker = () => {
@@ -91,7 +131,6 @@ const emptyChecker = () => {
         emptyError.style.display = 'flex'
     }
 }
-
 
 const loaderLoading = () => {
     window.addEventListener('load', () => {
@@ -126,5 +165,18 @@ const dateCalculating = () => {
     hour = today.getHours() + ':' + (today.getMinutes())
     timer.textContent = date
 }
+
+ScrollReveal({
+    reset: false,
+    distance: '180px',
+    duration: 1500
+})
+
+ScrollReveal().reveal('.scroll-box1', { delay: 1100, origin: 'left' })
+ScrollReveal().reveal('.nav__title-box--line', { delay: 1300, origin: 'left' })
+ScrollReveal().reveal('.scroll-box2', { delay: 1600, origin: 'left' })
+ScrollReveal().reveal('.nav__options', { delay: 1700, origin: 'left' })
+ScrollReveal().reveal('.container', { delay: 1800, origin: 'left' })
+ScrollReveal().reveal('.history-container', { delay: 2000, origin: 'left' })
 
 document.addEventListener('DOMContentLoaded', main)
